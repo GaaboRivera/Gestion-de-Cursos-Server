3562bc9c095296ff05c2e4e4877b6750fa0835bd
Gabriel Rivera <gabo.grm27@gmail.com>
2023-12-14 17:21:00 -0600

    AccessToken y refreshToken

--- constants.js
+++ constants.js
@@ -5,10 +5,13 @@ const DB_HOST = "websistemacursos.3poc7xl.mongodb.net";
 const API_VERSION = "v1";
 const IP_SERVER = "localhost";
 
+const JWT_SECRET_KEY = "kbwiwqbnweicbsduisdksop";
+
 module.exports = {
   DB_HOST,
   DB_USER,
   DB_USER_PASSWORD,
   API_VERSION,
   IP_SERVER,
+  JWT_SECRET_KEY
 };
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
+const jwt = require("../utils/jwt.js");
 
 function register(req, res) {
   const { firstname, lastname, email, password } = req.body;
@@ -14,7 +15,7 @@ function register(req, res) {
     lastname,
     email: email.toLowerCase(),
     role: "user",
-    actives: false
+    active: false
   });
 
   const salt = bcrypt.genSaltSync(10);
@@ -22,13 +23,6 @@ function register(req, res) {
 
   user.password = hashPassword;
 
-  // user.save((error, userStorage) => {
-  //   if (error) {
-  //     return res.status(400).send({ msg: "Error al crear el usuario" });
-  //   } else {
-  //     res.status(200).send(userStorage);
-  //   }
-  // });
   user
     .save()
     .then((response) => {
@@ -40,6 +34,54 @@ function register(req, res) {
     });
 }
 
+function login(req, res) {
+  const { email, password } = req.body;
+  if (!email) res.status(400).send({ msg: "El email es obligatorio" });
+  if (!password) res.status(400).send({ msg: "El password es obligatorio" });
+
+  const emailLowerCase = email.toLowerCase();
+  User.findOne({ email: emailLowerCase })
+    .then((userStore) => {
+      bcrypt.compare(password, userStore.password, (bcryptError, check) => {
+        if (bcryptError) {
+          return res.status(500).send({ msg: "Error del servidor" });
+        }
+        if (!check) {
+          return res.status(400).send({ msg: "Contraseña incorrecta" });
+        }
+        if (!userStore.active) {
+          return res
+            .status(401)
+            .send({ msg: "Usuario no autorizado o no activo" });
+        }
+        return res.status(200).send({
+          access: jwt.createAccessToken(userStore),
+          refresh: jwt.createRefreshToken(userStore)
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send({ msg: "Error en el servidor" });
+    });
+}
+
+function refreshAccessToken(req, res) {
+  const { token } = req.body;
+  if (!token) res.status(400).send({ msg: "Token requerido" });
+  const { user_id } = jwt.decoded(token);
+
+  User.find({ _id: user_id })
+    .then((userStore) => {
+      res.status(200).send({ accessToken: jwt.createAccessToken(userStore) });
+    })
+    .catch((err) => {
+      res.status(500).send({ msg: "Error en el servidor" });
+    });
+}
+
 module.exports = {
-  register
+  register,
+  login,
+  refreshAccessToken
 };
